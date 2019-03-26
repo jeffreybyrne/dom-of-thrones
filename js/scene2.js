12f9f6ec568be629e549abe325775e0001a4173c
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-03-26 15:59:09 -0400

    started adding in joffrey

--- js/scene2.js
+++ js/scene2.js
@@ -9,8 +9,38 @@ function scene2() {
   // Setup Cast
 
   // Character 1
-
+  const nedContainer = document.createElement('div');
+  stage.append(nedContainer);
+  nedContainer.style.position = 'absolute';
+  nedContainer.style.width = '10%';
+  const ned = document.querySelector('#ned');
+  nedContainer.append(ned);
+  const leather = document.querySelector('#leather-armour');
+  nedContainer.append(leather);
+  ned.style.width = '70%';
+  ned.style.position = 'absolute';
+  ned.style.top = 0;
+  leather.style.position = 'absolute';
+  leather.style.bottom = 0;
+  nedContainer.style.height = '37%';
+  nedContainer.style.top = '55%';
+  nedContainer.style.left = '25%';
+  ned.style.border = 'none';
+  leather.style.border = 'none';
+  nedContainer.style.border = 'none';
+  nedContainer.style.background = 'none';
   // Character 2
+  const joffContainer = document.createElement('div');
+  stage.append(joffContainer);
+  joffContainer.style.position = 'absolute';
+  joffContainer.style.width = '10%';
+  joffContainer.style.top = '10%';
+  const joff = document.querySelector('#joffrey');
+  joffContainer.append(joff);
+  joffContainer.style.border = '2px solid red';
+  joffContainer.style.background = 'yellow';
+  const king = document.querySelector('#king');
+  joffContainer.append(king);
 
   // Setup Props
 
