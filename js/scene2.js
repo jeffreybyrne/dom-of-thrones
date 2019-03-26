94ea17f8f4165bb1f2c40f0b3c5d9e0b7247f69d
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-03-26 15:45:49 -0400

    setting up the stage for scene 2

---
+++ js/scene2.js
@@ -0,0 +1,35 @@
+function scene2() {
+  console.log('playing scene 2')
+  // Setup Stage
+  const stage = document.querySelector('#stage');
+  const castle = document.querySelector('#castle');
+  const stageImg = stage.querySelector('#stage-img');
+  const castleImgSrc = castle.getAttribute('src');
+  stageImg.setAttribute('src', castleImgSrc);
+  // Setup Cast
+
+  // Character 1
+
+  // Character 2
+
+  // Setup Props
+
+  // Prop 1
+
+  // Prop 2
+
+  // Action!
+
+  // Stage direction 1
+
+  frame(() => {
+
+  });
+
+  // Stage direction 2
+
+  frame(() => {
+
+  });
+
+}
